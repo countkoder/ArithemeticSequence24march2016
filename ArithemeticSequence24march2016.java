ec7926508a986556aa3d42dff307875dd4bd5e3a
countkoder <sasha.farenyuk@gmail.com>
2016-03-24 18:19:54 -0400

    Step 2.
    Version 2: let the user enter the numbers instead of hard coded 1 and 20.

--- ArithemeticSequence24march2016.java
+++ ArithemeticSequence24march2016.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class ArithemeticSequence24march2016 {
-// version 1.    
-//add numbers from 1 to 20.
+// version 1: add numbers from 1 to 20.
+// version 2: let the user enter the numbers instead of hardcoded 1 and 20.
     public static void main(String[] args) {
-        int sum = 0;
-        for(int i=1; i<=20; i++) {
-             sum = sum + i;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("-------Arithemetic Sequence-------\n");
+        System.out.println("Enter the first element:");
+        double a1 = scan.nextDouble();
+        System.out.println("Enter the number of elements:");
+        int n = scan.nextInt();
+        double an = a1;
+        double sum = a1;
+        for(int i=2; i<=n; i++) {
+             an = an + 1;
+             sum=sum+an;
         }
         System.out.println(sum);
     }

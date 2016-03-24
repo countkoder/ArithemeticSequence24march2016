3c8aa331245a7dec61494bd33c59e24f44f22ed2
countkoder <sasha.farenyuk@gmail.com>
2016-03-24 18:42:53 -0400

    Step 3.
    version 3: let the user choose the difference instead of diff=1.

--- ArithemeticSequence24march2016.java
+++ ArithemeticSequence24march2016.java
@@ -2,19 +2,22 @@ import java.util.Scanner;
 public class ArithemeticSequence24march2016 {
 // version 1: add numbers from 1 to 20.
 // version 2: let the user enter the numbers instead of hardcoded 1 and 20.
+// version 3: let the user choose the difference instead of diff=1.
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("-------Arithemetic Sequence-------\n");
         System.out.println("Enter the first element:");
         double a1 = scan.nextDouble();
+        System.out.println("Enter the difference:");
+        double d = scan.nextDouble();
         System.out.println("Enter the number of elements:");
         int n = scan.nextInt();
         double an = a1;
         double sum = a1;
         for(int i=2; i<=n; i++) {
-             an = an + 1;
-             sum=sum+an;
+             an += d;
+             sum += an;
         }
-        System.out.println(sum);
+        System.out.println("Sum = " + sum);
     }
 }

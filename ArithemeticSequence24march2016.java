38e4313a3efbe3dfa67dc2dea2e680ef983c053e
countkoder <sasha.farenyuk@gmail.com>
2016-03-24 17:35:32 -0400

    Step 1: add numbers from 1 to 20.

---
+++ ArithemeticSequence24march2016.java
@@ -0,0 +1,11 @@
+public class ArithemeticSequence24march2016 {
+// version 1.    
+//add numbers from 1 to 20.
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i=1; i<=20; i++) {
+             sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}

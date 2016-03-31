6d648524b3d44aa936bb769cfd74951fd94552cf
Sasha Farenyuk <sasha.farenyuk@gmail.com>
2016-03-31 17:41:44 -0400

    Added files via upload
    version 1.list all the n elements of the sequence

---
+++ GeometricSum1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class GeometricSum1 {
+    public static void main(String[] args){
+        //version 2.compare the loop result and the formula result
+        System.out.println("-------Geometric sequence-------");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first element:");
+        double b1 = scan.nextDouble();
+        System.out.println("Enter the common ratio:");
+        double r = scan.nextDouble();
+        System.out.println("Enter the number of elements:");
+        int n = scan.nextInt();
+        double sum = b1;
+        double bn = b1;
+        double sumFormula =b1 * (Math.pow(r,n)-1)/(r-1); 
+        for(int i=1; i<=n; i++) {
+            if(i>1) bn=bn*r;
+            System.out.println("a(" + i + ") = " + bn);
+            if(i>1) sum = sum + bn; 
+        }
+        System.out.println("Using math foumula: sum(" + n + ") = " + sumFormula);
+        System.out.println("Using loop:         sum(" + n + ") = " + sum);
+    }
+}

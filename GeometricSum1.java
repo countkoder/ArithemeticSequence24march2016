959e13b30a6c75e478dad3bfff0c6477fe5d90bf
Sasha Farenyuk <sasha.farenyuk@gmail.com>
2016-03-31 19:54:19 -0400

    Added files via upload
    version 4.implementing using input parameters

--- GeometricSum1.java
+++ GeometricSum1.java
@@ -1,23 +1,25 @@
-import java.util.Scanner;
 class GeometricSum1 {
     public static void main(String[] args){
-        //version 3.listing the first five results
-        System.out.println("-------Geometric sequence-------");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the first element:");
-        double b1 = scan.nextDouble();
-        System.out.println("Enter the common ratio:");
-        double r = scan.nextDouble();
+        //version 4.implementing using input parameters
+//        System.out.println("Enter the first element:");
+//        double b1 = scan.nextDouble();
+//        System.out.println("Enter the common ratio:");
+//        double r = scan.nextDouble();
+//        System.out.println("Enter the number of elements:");
+//        int n = scan.nextInt();
+        double b1 = Double.parseDouble(args[0]);
+        double r = Double.parseDouble(args[1]);
+        double n = Double.parseDouble(args[2]);
         double sum = b1;
         double bn = b1;
-        double sumFormula =b1 * (Math.pow(r,5)-1)/(r-1); 
-        for(int i=1; i<=5; i++) {
+        double sumFormula =b1 * (Math.pow(r,n)-1)/(r-1); 
+        for(int i=1; i<=n; i++) {
             if(i>1) bn=bn*r;
             System.out.println("a(" + i + ") = " + bn);
             if(i>1) sum = sum + bn; 
         }
-        System.out.println("Using math foumula: sum(" + 5 + ") = " + sumFormula);
-        System.out.println("Using loop:         sum(" + 5 + ") = " + sum);
+        System.out.println("Using math foumula: sum(" + n + ") = " + sumFormula);
+        System.out.println("Using loop:         sum(" + n + ") = " + sum);
         if(sumFormula != sum) System.out.println("INCORRECT");
         else System.out.println("GOOD WORK");
     }

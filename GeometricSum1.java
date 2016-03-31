09b4a5679460fd95f8e8d8b4015916867055ce6a
Sasha Farenyuk <sasha.farenyuk@gmail.com>
2016-03-31 19:24:16 -0400

    Added files via upload
    version 3.listing the first five results

--- GeometricSum1.java
+++ GeometricSum1.java
@@ -1,25 +1,23 @@
 import java.util.Scanner;
 class GeometricSum1 {
     public static void main(String[] args){
-        //version 2.comparing the results of the loop and the formula
+        //version 3.listing the first five results
         System.out.println("-------Geometric sequence-------");
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the first element:");
         double b1 = scan.nextDouble();
         System.out.println("Enter the common ratio:");
         double r = scan.nextDouble();
-        System.out.println("Enter the number of elements:");
-        int n = scan.nextInt();
         double sum = b1;
         double bn = b1;
-        double sumFormula =b1 * (Math.pow(r,n)-1)/(r-1); 
-        for(int i=1; i<=n; i++) {
+        double sumFormula =b1 * (Math.pow(r,5)-1)/(r-1); 
+        for(int i=1; i<=5; i++) {
             if(i>1) bn=bn*r;
             System.out.println("a(" + i + ") = " + bn);
             if(i>1) sum = sum + bn; 
         }
-        System.out.println("Using math foumula: sum(" + n + ") = " + sumFormula);
-        System.out.println("Using loop:         sum(" + n + ") = " + sum);
+        System.out.println("Using math foumula: sum(" + 5 + ") = " + sumFormula);
+        System.out.println("Using loop:         sum(" + 5 + ") = " + sum);
         if(sumFormula != sum) System.out.println("INCORRECT");
         else System.out.println("GOOD WORK");
     }
